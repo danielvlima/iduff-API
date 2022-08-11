bcfa6221a385b91f5510cb03df91397c6d3cbfec
Daniel Lima <daniel.verginio@injunior.com.br>
2022-08-11 00:41:18 -0300

    first commit

---
+++ src/modules/Users/model/User.ts
@@ -0,0 +1,16 @@
+
+
+
+class User {
+    name: string;
+    nacionality: string;
+    state: string;
+    RG: string;
+    cpf: string;
+    email: string;
+    password: string;
+    birthDate: Date;
+    type: string; //pesquisar enum typeorm
+}
+
+export { User };
